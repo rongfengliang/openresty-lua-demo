c818b41addfcaf98b0d1b3afc542a487a00a2824
dalongrong <1141591465@qq.com>
2018-10-19 21:28:36 +0800

    add some var dmeo

---
+++ app/vars/vars.lua
@@ -0,0 +1,18 @@
+local json = require("cjson")
+local function vars()
+    -- body
+    local str = ngx.req.raw_header()
+    local result = {
+        a= ngx.var.arg_a,
+       -- h=str,
+        org=ngx.headers,
+        token=ngx.req.get_headers()["token"]
+    };
+    if result.token ~="dalong" then
+        ngx.say("not a validate token")
+    else
+        ngx.say(result.token..result.a)
+    end
+    --ngx.say(json.encode(result))
+end
+return vars
